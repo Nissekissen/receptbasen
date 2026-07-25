c74eb355bfe147466bdaccb3cfb3a0f92c80b7af
Nils Lindblad <nils.m.lindblad@gmail.com>
2026-07-25 22:37:28 +0200

    added filtering to /recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,8 +21,17 @@ class RecipesController < ApplicationController
   end
 
   def index
-    # @recipes = Current.user.recipes.distinct
-    @recipes = Recipe.all
+    @recipes = Current.user.recipes.distinct
+    # @recipes = Recipe.all
+    @recipes = @recipes.where(id: SavedRecipe.where(collection_id: params[:collection_id]).select(:recipe_id)) if params[:collection_id].present?
+    @recipes = @recipes.where(id: Tagging.where(tag_id: params[:maltidstyp_tag_id]).select(:recipe_id)) if params[:maltidstyp_tag_id].present?
+    @recipes = @recipes.where(id: Tagging.where(tag_id: params[:kok_tag_id]).select(:recipe_id)) if params[:kok_tag_id].present?
+    @recipes = @recipes.where(id: Tagging.where(tag_id: params[:kost_tag_id]).select(:recipe_id)) if params[:kost_tag_id].present?
+
+    @collections = Current.user.collections
+    @maltidstyp_tags = Tag.maltidstyp.order(:name)
+    @kok_tags = Tag.kok.order(:name)
+    @kost_tags = Tag.kost.order(:name)
   end
 
   def destroy
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -29,4 +29,13 @@ module RecipesHelper
   def recipe_difficulty_icon(difficulty)
     { "lätt" => "easy", "medel" => "medium", "avancerad" => "hard" }[difficulty&.downcase]
   end
+
+  def active_recipe_filters
+    filters = []
+    filters << { param: :collection_id, label: Current.user.collections.find_by(id: params[:collection_id])&.name } if params[:collection_id].present?
+    filters << { param: :maltidstyp_tag_id, label: Tag.find_by(id: params[:maltidstyp_tag_id])&.name&.capitalize } if params[:maltidstyp_tag_id].present?
+    filters << { param: :kok_tag_id, label: Tag.find_by(id: params[:kok_tag_id])&.name&.capitalize } if params[:kok_tag_id].present?
+    filters << { param: :kost_tag_id, label: Tag.find_by(id: params[:kost_tag_id])&.name&.capitalize } if params[:kost_tag_id].present?
+    filters.select { |filter| filter[:label].present? }
+  end
 end
