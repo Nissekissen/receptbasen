9859020a789e3ba652a07a00098af1c486c80f99
Nils Lindblad <nils.m.lindblad@gmail.com>
2026-08-08 21:49:49 +0200

    updated cook mode view

--- app/javascript/controllers/disclosure_controller.js
+++ app/javascript/controllers/disclosure_controller.js
@@ -3,8 +3,25 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["trigger"]
 
+  connect() {
+    this.hideBound = this.hide.bind(this)
+    document.addEventListener("click", this.hideBound)
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.hideBound)
+  }
+
   toggle() {
-    const isOpen = this.element.classList.toggle("is-open")
+    this.setOpen(!this.element.classList.contains("is-open"))
+  }
+
+  hide(event) {
+    if (!this.element.contains(event.target)) this.setOpen(false)
+  }
+
+  setOpen(isOpen) {
+    this.element.classList.toggle("is-open", isOpen)
     this.triggerTarget.setAttribute("aria-expanded", isOpen)
   }
 }
