173dc089e4715b9db7b9d4f451514b8d9ed8979c
Nils Lindblad <nils.m.lindblad@gmail.com>
2026-08-11 23:02:53 +0200

    updated links to home page/explore page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "shows the landing page to authenticated users" do
-    sign_in_as(users(:one))
+  test "shows the landing page to unauthenticated visitors" do
     get home_index_url
     assert_response :success
   end
 
-  test "redirects unauthenticated visitors to explore" do
+  test "redirects authenticated users to explore" do
+    sign_in_as(users(:one))
     get home_index_url
     assert_redirected_to explore_path
   end
