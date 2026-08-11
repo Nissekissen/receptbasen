d5e16d2822392c679006f550465f3edce5a67f0b
Nils Lindblad <nils.m.lindblad@gmail.com>
2026-08-11 09:40:52 +0200

    added shelves to frontend

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -8,5 +8,12 @@ class ExploreController < ApplicationController
     @kok_tags = Tag.kok.order(:name)
     @kok_tag = Tag.kok.find_by(id: params[:kok_tag_id])
     @results = Recipe.catalog.where(id: Tagging.where(tag: @kok_tag).select(:recipe_id)).order_by_popularity unless @kok_tag.nil?
+    @shelves = [
+      { title: "Populärt just nu", type: "big", recipes: Recipe.catalog.order_by_popularity(3) },
+      { title: "Snabbt & enkelt", type: "row", recipes: Recipe.catalog.order_by_popularity(10) },
+      { title: "Veckans favorit", type: "split", recipes: Recipe.catalog.order_by_popularity(5) },
+      { title: "Nyss tillagda", type: "list", recipes: Recipe.catalog.order_by_popularity(8) },
+      { title: "Helgmys eller vardagsmat?", type: "duo", groups: [{ label: "Helgmys", recipes: Recipe.catalog.order_by_popularity(4) }, { label: "Vardagsmat", recipes: Recipe.catalog.order_by_popularity(4) }] }
+    ]
   end
 end
